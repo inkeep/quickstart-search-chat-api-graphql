57fbce7e092755457bf283d142b3c84a416b525e
Nick Gomez <nick@inkeep.com>
2023-08-09 19:09:55 -0400

    using genql, no typing on response for some reason

---
+++ README.md
@@ -0,0 +1,11 @@
+# quick-start-search-chat-api
+
+## Configure tenant to be created
+
+Edit the `inputParams.ts` file with the needed config for a tenant
+
+## run ts-node command
+
+`yarn run`
+
+## this will create: Org, Project, and Sandbox
---
+++ src/generateInkeepGraphQLSdl.ts
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import { printSchema, buildClientSchema, GraphQLSchema, getIntrospectionQuery } from 'graphql';
+import fs from 'fs';
+
+const introspectionQuery = getIntrospectionQuery();
+
+async function fetchSchema(endpoint: string): Promise<string> {
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: introspectionQuery }),
+  });
+
+  const jsonResponse: any = await response.json();
+
+  if (!jsonResponse.data) {
+    throw new Error('No data received from endpoint.');
+  }
+
+  const schema: GraphQLSchema = buildClientSchema(jsonResponse.data);
+
+  return printSchema(schema);
+}
+
+const endpoint = 'https://api.inkeep.com/graphql';
+
+fetchSchema(endpoint).then((schemaSDL) => {
+  fs.writeFileSync('./inkeep-api-sdl.graphql', schemaSDL);
+  console.log('Schema has been saved to inkeep-api-sdl.graphql');
+}).catch((error) => {
+  console.error('An error occurred:', error);
+});
